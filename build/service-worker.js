b32003d473c2c929f524a8c785bf6bb49379ea46
kahdichienja <agooclinton@gmail.com>
2020-07-12 18:06:51 +0300

    funding

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a47e14776163e43669dc53daf86a8bba.js"
+  "/precache-manifest.f7dfbbec507b39993a1b51e6c051b4e7.js"
 );
 
 workbox.clientsClaim();
